33c01041d265ac282d7b69cfd38ddf2020ab191a
Ted Bowman <ted.git@tedbow.com>
2017-08-29 15:59:47 -0400

    added test count()

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Drupal\tester\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
 
 /**
  * Class DefaultController.
@@ -24,6 +25,29 @@ class DefaultController extends ControllerBase {
       '#type' => 'markup',
       '#markup' => $this->t('Implement method: message')
     ];
+  }
+
+  public function testCount() {
+    $node = Node::load(1);
+
+    $markup = 'results1: ';
+    $markup .=  ($node->get('path')->isEmpty() ? 'isempty': 'no isempty') .  '----';
+    $markup .=  (count($node->get('path')) ? 'count': 'no count') .  '----';
+    /** @var EntityTypeManagerInterface $manager */
+    $manager = \Drupal::service('entity_type.manager');
+    $manager->getStorage('node')->resetCache([1]);
+    $node = Node::load(1);
+    $markup .= '<br>results2: ';
+    $markup .=  (count($node->get('path')) ? 'count': 'no count') .  '----';
+    $markup .=  ($node->get('path')->isEmpty() ? 'isempty': 'no isempty') .  '----';
+
+
+
+    return [
+      '#type' => 'markup',
+      '#markup' => $markup,
+    ];
+
   }
 
 }
